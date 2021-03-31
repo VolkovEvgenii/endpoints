cd2d6c958a3e2d9607ac7eb5136084adbadde9a2
evgeniy volkov <volkov.e.n@gmail.com>
2021-03-31 18:11:47 +0300

    init

---
+++ src/main/java/ru/volkov/integration/endpoints/EndpointsApplication.java
@@ -0,0 +1,13 @@
+package ru.volkov.integration.endpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndpointsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/volkov/integration/endpoints/EndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volkov.integration.endpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
