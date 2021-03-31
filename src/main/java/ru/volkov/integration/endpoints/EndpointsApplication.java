5de52bd462a6a771e57cf5bc50c3e4ae4602eb3f
evgeniy volkov <volkov.e.n@gmail.com>
2021-03-31 18:14:49 +0300

    add integration_context.xml

--- src/main/java/ru/volkov/integration/endpoints/EndpointsApplication.java
+++ src/main/java/ru/volkov/integration/endpoints/EndpointsApplication.java
@@ -2,8 +2,10 @@ package ru.volkov.integration.endpoints;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:integration_context.xml")
 public class EndpointsApplication {
 
     public static void main(String[] args) {
