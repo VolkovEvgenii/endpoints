87240fc7b8a827b3c4815f82d0acb4f70bb5801d
evgeniy volkov <volkov.e.n@gmail.com>
2021-04-01 12:38:14 +0300

    fix splitter

--- src/main/java/ru/volkov/integration/endpoints/CustomSplitter.java
+++ src/main/java/ru/volkov/integration/endpoints/CustomSplitter.java
@@ -1,15 +1,13 @@
 package ru.volkov.integration.endpoints;
 
-import org.springframework.integration.splitter.AbstractMessageSplitter;
 import org.springframework.messaging.Message;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class CustomSplitter extends AbstractMessageSplitter {
+public class CustomSplitter {
 
-    @Override
-    protected Object splitMessage(Message<?> message) {
+    protected Object split(Message<?> message) {
         return new ArrayList<String>(Arrays.asList(message.getPayload().toString().split(" ")));
     }
 }
